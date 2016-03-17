22e3b10a5650624691f8a141e44eda069b7dc7c3
comerc <comerc@mail.ru>
2016-03-17 16:13:20 +0300

    support multi-line

--- autoform-contenteditable2.js
+++ autoform-contenteditable2.js
@@ -7,10 +7,59 @@
 // TODO: манипуляция выделением https://github.com/JackAdams/meteor-editable-text-wysiwyg/blob/master/lib/wysiwyg.js#L9
 // TODO: hot keys https://github.com/jeresig/jquery.hotkeys
 
+var matched, browser;
+
+jQuery.uaMatch = function( ua ) {
+    ua = ua.toLowerCase();
+
+    var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+        /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+        /(msie)[\s?]([\w.]+)/.exec( ua ) ||
+        /(trident)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+        ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+        [];
+
+    return {
+        browser: match[ 1 ] || "",
+        version: match[ 2 ] || "0"
+    };
+};
+
+matched = jQuery.uaMatch( navigator.userAgent );
+//IE 11+ fix (Trident)
+matched.browser = matched.browser == 'trident' ? 'msie' : matched.browser;
+browser = {};
+
+if ( matched.browser ) {
+    browser[ matched.browser ] = true;
+    browser.version = matched.version;
+}
+
+// Chrome is Webkit, but Webkit is also Safari.
+if ( browser.chrome ) {
+    browser.webkit = true;
+} else if ( browser.webkit ) {
+    browser.safari = true;
+}
+
+jQuery.browser = browser;
+
 AutoForm.addInputType("contenteditable2", {
   template: "afContenteditable2",
+  valueIn: function (value) {
+    return value.replace(/\n/g, "<br>");
+  },
   valueOut: function () {
-    return this.html();
+    // return this.html();
+    var $ce = $("<pre />").html(this.html());
+    if ($.browser.webkit)
+      $ce.find("div").replaceWith(function() { return "\n" + this.innerHTML; });
+    if ($.browser.msie)
+      $ce.find("p").replaceWith(function() { return this.innerHTML + "<br>"; });
+    // if ($.browser.mozilla || $.browser.opera || $.browser.msie)
+      $ce.find("br").replaceWith("\n");
+    return $ce.text();
   },
   contextAdjust: function (context) {
     if (typeof context.atts["data-maxlength"] === "undefined" && typeof context.max === "number") {
